7273849e8d262a392e217d9740f6e85f8980fc5f
CodeXcellent Education <33389345+codexcellent-education@users.noreply.github.com>
2021-05-10 01:07:45 +0300

    server

---
+++ index.js
@@ -0,0 +1,17 @@
+//imported packages
+const express = require('express');
+
+//create express server
+const app = express();
+
+//Routes
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+//Setup Port Number
+const port = process.env.PORT || 4000;
+
+//Start Server
+const server = app.listen(port, function(){
+    console.log('Server Listening on Port: ' +port);
+});
